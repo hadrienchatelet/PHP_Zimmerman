754425e10cdf23b7cf6569572416dfc5947d8319
Hadrien Chatelet <hadrien_chatelet@ens.univ-artois.fr>
2017-11-06 14:30:14 +0100

    add IndexController

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,21 +3,29 @@
  * Created by PhpStorm.
  * User: hadrienchatelet
  * Date: 06/11/2017
- * Time: 13:32
+ * Time: 14:23
  */
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
-class DefaultController extends Controller
+class IndexController extends Controller
 {
+    /**
+     *  @Route("/", name="hello_world")
+     */
     public function index()
     {
+        /*
         return new Response(
             'Hello World !'
         );
-
+        */
+        return $this->render(
+            'Index/index.html.twig',[]
+        );
     }
 }
